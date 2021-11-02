c1b0fb70e46e11455ca647bcfd415e1e0ba47f48
AndrewH1188 <andrewh1188@gmail.com>
2021-11-02 09:48:01 +0000

    Added code not used into test file to save confusion, but to refer to if needed

---
+++ test.py
@@ -0,0 +1,26 @@
+"""
+The below code is commented out code as this has been refactored 
+and used elsewhere just neater and easier to understand
+"""
+#def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    #print("Updating sales worksheet...\n")
+    #sales_worksheet = SHEET.worksheet("sales")
+    #sales_worksheet.append_row(data)
+    #print("Sales worksheet updated successfully.\n")
+
+
+#def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the list data provided.
+    """
+    #print("Updating surplus worksheet...\n")
+    #surplus_worksheet = SHEET.worksheet("surplus")
+    #surplus_worksheet.append_row(data)
+    #print("Surplus worksheet updated successfully.\n")
+"""
+The above code is commented out code as this has been refactored 
+and used elsewhere just neater and easier to understand
+"""
