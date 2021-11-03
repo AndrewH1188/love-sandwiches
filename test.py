0afa4fc3c41d3e01f3e11599abce6b14d85b211b
AndrewH1188 <andrewh1188@gmail.com>
2021-11-03 11:13:36 +0000

    Added the results from the challenge model solution for reference

--- test.py
+++ test.py
@@ -24,3 +24,35 @@ and used elsewhere just neater and easier to understand
 The above code is commented out code as this has been refactored 
 and used elsewhere just neater and easier to understand
 """
+
+
+
+
+
+
+
+
+
+# MODEL SOLUTION TO THE CHALLENGE BELOW:
+
+
+# student writes function
+def get_stock_values(data):
+    """
+    Print out the calculated stock numbers for each sandwich type.
+    """
+    headings = SHEET.worksheet("stock").get_all_values()[0]
+
+    # headings = SHEET.worksheet('stock').row_values(1)
+
+    print("Make the following numbers of sandwiches for next market:\n")
+
+    # new_data = {}
+    # for heading, stock_num in zip(headings, data):
+    #     new_data[heading] = stock_num
+    # return new_data
+    
+    return {heading: data for heading, data in zip(headings, data)}
+    
+stock_values = get_stock_values(stock_data)
+print(stock_values)
